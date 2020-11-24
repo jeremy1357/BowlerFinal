87f5901fd23967325b9cab6de38a179a3815a0c4
Demo User <debian@beaglebone.localdomain>
2020-11-24 15:58:11 +0000

    Updated

--- final.cpp
+++ final.cpp
@@ -7,20 +7,20 @@
 
 using namespace exploringBB;
 
-const float V_PER_STEP = 1.80f / 4096.0f;
+const double V_PER_STEP = 1.80 / 4096.0;
 
 
 
 float convertToTempF(int ADCvalue) {
-float x = ((ADCvalue * (1.80f / 4096.0f)) - 0.75f) / 0.01f;
+float x = ((ADCvalue * (1.80 / 4096.0)) - 0.75) / 0.01;
 return (((x + 25.0f) * 9) / 5) + 32 ;
 }
 
 
 
 // USE ANALOG INPUT 6
-float convertToVoltage(int adcStep) {
-return V_PER_STEP * (float)adcStep;
+double convertToVoltage(int adcStep) {
+return V_PER_STEP * (double)adcStep;
 }
 
 int main(int argc, char* argv[]) {
@@ -33,18 +33,20 @@ pushbutton.setDirection(INPUT);
 
 // AIN0 connected to TMP36
 // AIN6 connected to voltage divider
-AnalogIn temperature(0);
-AnalogIn voltageDivider(6);
-
-
 int pushbuttonReading = pushbutton.getValue();
 std::cout << "Button reading " << pushbuttonReading << std::endl;
+
+
 if (pushbuttonReading == 0) {
+AnalogIn voltageDivider(0);
+
+
 std::cout << "Button is pushed!" << std::endl;
 std::cout << "Displaying resistance and LED will be blinked twice" << std::endl;
 int ain0Reading = voltageDivider.readADCsample();
-float voltage = convertToVoltage(ain0Reading);
-float resistance = (voltage * 10000.0f) / (1.8f - voltage);
+std::cout << "Reading " << ain0Reading << std::endl;
+double voltage = convertToVoltage(ain0Reading);
+double resistance = (voltage * 10000) / (1.8 - voltage);
 std::cout << "Calculated resistance = " << resistance << std::endl;
 
 led.setValue(HIGH);
@@ -58,6 +60,7 @@ led.setValue(LOW);
 }
 else if (pushbuttonReading == 1)
 {
+AnalogIn temperature(6);
 std::cout << "Button is not pressed!" << std::endl;
 std::cout << "Reading temperature and blinking LED once" << std::endl;
 std::cout << "Raw temperature is " << temperature.readADCsample() << std::endl;
